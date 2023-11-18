610ab986ccb721efc26d05cb480ce8037dc3dce1
jk.yang <35951244+yangjiakai@users.noreply.github.com>
2023-11-18 22:40:52 +0900

    统一主题颜色

--- app/[locale]/admin/dashboard/page.js
+++ app/[locale]/admin/dashboard/page.js
@@ -1,7 +1,10 @@
+"use client";
 function DashBoard() {
-  return (<div>
-    <h1>dashboard</h1>
-  </div>);
+  return (
+    <div>
+      <h1>dashboard</h1>
+    </div>
+  );
 }
 
 export default DashBoard;
--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,11 +1,10 @@
-
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { notFound } from 'next/navigation';
-import { NextIntlClientProvider } from 'next-intl';
-
-const locales = ['en', 'jp'];
-const inter = Inter({ subsets: ['latin'] })
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { notFound } from "next/navigation";
+import { NextIntlClientProvider } from "next-intl";
+import { NextUIProvider } from "@nextui-org/react";
+const locales = ["en", "jp"];
+const inter = Inter({ subsets: ["latin"] });
 
 export default async function LocaleLayout({ children, params: { locale } }) {
   let messages;
@@ -16,15 +15,14 @@ export default async function LocaleLayout({ children, params: { locale } }) {
   }
 
   return (
-    <html lang={locale}>
-      <body>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-
-          <main>
-            {children}
-          </main>
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <NextUIProvider>
+      <html lang={locale}>
+        <body>
+          <NextIntlClientProvider locale={locale} messages={messages}>
+            <main>{children}</main>
+          </NextIntlClientProvider>
+        </body>
+      </html>
+    </NextUIProvider>
   );
 }
